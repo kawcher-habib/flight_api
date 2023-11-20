c14416c8c7f9530e51321b80ab9a32d49d3b4644
habibflyfar <habib@flyfarint.com>
2023-11-21 00:15:02 +0600

    try agein

--- project-root/src/routes/airsearch.js
+++ project-root/src/routes/airsearch.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get("/flight", ()=>{
-    console.log("Hello world i am from  flight route api");
+    console.log("Hello world i am from  flight route test api");
 });
 
 module.exports = routes;
